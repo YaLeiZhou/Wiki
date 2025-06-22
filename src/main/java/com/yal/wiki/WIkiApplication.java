a7e18c3fde0ff631539f4f501c41df2442d973c7
周亚雷 <1069763236@qq.com>
2025-06-22 18:23:44 +0800

    2.2 project build

---
+++ src/main/java/com/yal/wiki/WIkiApplication.java
@@ -0,0 +1,13 @@
+package com.yal.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WIkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WIkiApplication.class, args);
+    }
+
+}
