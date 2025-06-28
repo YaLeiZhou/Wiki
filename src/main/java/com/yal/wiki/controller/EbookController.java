b8fefc2b8ef9f7c0642f983faa0bdbb1ad49b888
周亚雷 <1069763236@qq.com>
2025-06-28 15:53:12 +0800

    3-6 完成电子书列表查询接口

---
+++ src/main/java/com/yal/wiki/controller/EbookController.java
@@ -0,0 +1,29 @@
+package com.yal.wiki.controller;
+
+
+import com.yal.wiki.pojo.Ebook;
+import com.yal.wiki.service.EbookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+    /**
+     * @RestController注解用于声明返回文本数据，一般返回JSON
+     * @Controller注解用于声明返回界面
+     * @RestController = @Controller + ResponseBody
+     */
+    @Autowired
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public List<Ebook> list(){
+        System.out.println(ebookService.list());
+        return ebookService.list();
+    }
+}
