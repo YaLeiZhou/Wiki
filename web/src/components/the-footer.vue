8b3a199865c02ce7c8995192c6567c9af473b3a8
周亚雷 <1069763236@qq.com>
2025-07-05 17:36:10 +0800

    4-7 制作the-footer组件，删除HelloWorld组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    v1@Copyright电子书 com.ya.wiki
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import{defineComponent} from "vue";
+
+export default  defineComponent(
+    {
+      name: "the-footer"
+    }
+)
+</script>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -52,8 +52,5 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 });
 </script>
